94e4f20ad0d08e990bb921a45a297dd279df5606
RefinedSoftwareLLC <mikar_gibbros@yahoo.com>
2016-03-23 03:33:18 -0600

    Update sh.bat

--- sh.bat
+++ sh.bat
@@ -1,8 +1,8 @@
 #!/bin/sh   #^
-#:Loading PowerShell:## 2>nul &@SETLOCAL EnableDelayedExpansion&echo off
+#:Loading PowerShell:## 2>nul &@setlocal enabledelayedexpansion&echo off
 # 2>nul &(if exist "%0" (copy "%0" "%0.ps1" /y >nul)else (copy "%0.bat" "%0.ps1" /y >nul))
-# 2>nul &powershell -ExecutionPolicy Bypass -noprofile -file "%0.ps1" %*&set error=!errorlevel!&del "%0.ps1"
-# 2>nul &(if "%0" neq "%~0" (echo.&echo Press any key to close...&pause >nul))&exit /b !error!
+# 2>nul &powershell -exec bypass -noprofile -file "%0.ps1" %*&set err=!errorlevel!&del "%0.ps1"
+# 2>nul &(if "%0" neq "%~0" (echo.&echo Press any key to close...&pause >nul))&exit /b !err!
 @'
 #' 2>/dev/null # sh.bat version 0.1 # DO NOT MODIFY THESE 7 LINES
 
@@ -26,7 +26,7 @@ exit 0
 #################
 
 # Change to path of script:
-cd (Split-Path $script:MyInvocation.MyCommand.Path) # for powershell 3.0+ use: cd $PSScriptRoot
+cd $PSScriptRoot
 echo "passed: $args"
 
 ################
