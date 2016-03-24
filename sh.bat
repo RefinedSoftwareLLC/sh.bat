23aa73483a48a9484050b3b27fa5c55887633069
RefinedSoftwareLLC <mikar_gibbros@yahoo.com>
2016-03-23 21:45:02 -0600

    Update sh.bat

--- sh.bat
+++ sh.bat
@@ -1,8 +1,8 @@
-#!/bin/sh   #^
-#:Loading PowerShell:## 2>nul &@setlocal enabledelayedexpansion&echo off&copy "%~nx0" "%~nx0.temp.ps1" /y >nul
-# 2>nul &powershell -exec bypass -noprofile -file "%~nx0.temp.ps1" %*&set err=!errorlevel!&del "%~nx0.temp.ps1"
-# 2>nul &(if "%0" neq "%~0" (echo.&echo Press any key to close...&pause >nul))&exit /b !err!
-@'
+#!/bin/sh  #^
+#:Loading PowerShell:## 2>nul&@setlocal enabledelayedexpansion&echo off&copy "%~nx0" "%~nx0.temp.ps1" /y >nul
+# 2>nul&powershell -exec bypass -noprofile -file "%~nx0.temp.ps1" %*&set err=!errorlevel!&del "%~nx0.temp.ps1"
+# 2>nul&(if "%0" neq "%~0" (echo.&echo Press any key to close...&pause >nul))&exit /b !err!
+[void]@'
 #' 2>/dev/null ### github.com/RefinedSoftwareLLC/sh.bat - v0.2 - DO NOT MODIFY THESE 6 LINES ###
 
 ################
@@ -17,8 +17,7 @@ echo "passed: $*"
 ### .sh end ###
 ###############
 
-exit 0
-'@|out-null ### DO NOT MODIFY THESE 2 LINES ###
+'@#' 2>/dev/null;exit 0 ### DO NOT MODIFY THIS LINE ###
 
 #################
 ### .ps1 mode ###
