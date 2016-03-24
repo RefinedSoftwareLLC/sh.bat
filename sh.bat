f16c734ce10ab7242f3cfca5bd2aceed3673675e
RefinedSoftwareLLC <mikar_gibbros@yahoo.com>
2016-03-23 20:47:46 -0600

    Update sh.bat

--- sh.bat
+++ sh.bat
@@ -1,10 +1,9 @@
 #!/bin/sh   #^
-#:Loading PowerShell:## 2>nul &@setlocal enabledelayedexpansion&echo off
-# 2>nul &(if exist "%0" (copy "%0" "%0.ps1" /y >nul)else (copy "%0.bat" "%0.ps1" /y >nul))
-# 2>nul &powershell -exec bypass -noprofile -file "%0.ps1" %*&set err=!errorlevel!&del "%0.ps1"
+#:Loading PowerShell:## 2>nul &@setlocal enabledelayedexpansion&echo off&copy "%~nx0" "%~nx0.temp.ps1" /y >nul
+# 2>nul &powershell -exec bypass -noprofile -file "%~nx0.temp.ps1" %*&set err=!errorlevel!&del "%~nx0.temp.ps1"
 # 2>nul &(if "%0" neq "%~0" (echo.&echo Press any key to close...&pause >nul))&exit /b !err!
 @'
-#' 2>/dev/null # sh.bat version 0.1 # DO NOT MODIFY THESE 7 LINES
+#' 2>/dev/null ### github.com/RefinedSoftwareLLC/sh.bat - v0.2 - DO NOT MODIFY THESE 6 LINES ###
 
 ################
 ### .sh mode ###
@@ -19,7 +18,7 @@ echo "passed: $*"
 ###############
 
 exit 0
-'@ | Out-Null # DO NOT MODIFY THESE 2 LINES
+'@ | out-null ### DO NOT MODIFY THESE 2 LINES ###
 
 #################
 ### .ps1 mode ###
