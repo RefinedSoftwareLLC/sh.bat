911d062d5d803ce29af6855e9f06ab4f91975f36
RefinedSoftwareLLC <mikar_gibbros@yahoo.com>
2016-03-23 23:30:38 -0600

    Update sh.bat

--- sh.bat
+++ sh.bat
@@ -2,8 +2,8 @@
 #:Loading PowerShell:## 2>nul&@setlocal enabledelayedexpansion&echo off&copy "%~nx0" "%~nx0.temp.ps1" /y >nul
 # 2>nul&powershell -exec bypass -noprofile -file "%~nx0.temp.ps1" %*&set err=!errorlevel!&del "%~nx0.temp.ps1"
 # 2>nul&(if "%0" neq "%~0" (echo.&echo Press any key to close...&pause >nul))&exit /b !err!
-[void]@'
-#' 2>/dev/null ### github.com/RefinedSoftwareLLC/sh.bat - v0.2 - DO NOT MODIFY THESE 6 LINES ###
+echo \' >/dev/null ' >$null;[void]@'
+### github.com/RefinedSoftwareLLC/sh.bat - v0.2 - DO NOT MODIFY THESE 6 LINES ###
 
 ################
 ### .sh mode ###
