d045c5ebc1cbe5a798d2d9caade0e43a7bc4fa54
RefinedSoftwareLLC <mikar_gibbros@yahoo.com>
2018-10-12 06:50:01 -0600

    prep for admin mode by making functions which bundle multiple files.

--- source/compile.cmd
+++ source/compile.cmd
@@ -17,97 +17,105 @@ REM .bat bootstrap
 REM start .ps1 skip over .sh
 call :addFileLn .\ps1.skip.try.txt
   REM .sh
-    call :addFile .\sh.try.txt
-    call :addFile .\version.txt
-      call :addStringLn " ###"
-      call :addFileLn .\sh.example.txt
-    REM call :addFileLn .\sh.finally.txt
+    call :sh
 REM end of .ps1 skip over .sh
 call :addFileLn .\ps1.skip.finally.txt
 REM .ps1
+  call :ps1
+
+goto :exit
+
+:sh
+setLocal
+  call :addFile .\sh.try.txt & call :addFile .\version.txt & call :addStringLn ". ###"
+  call :addFileLn .\sh.example.txt
+  REM call :addFileLn .\sh.finally.txt
+goto :return
+
+:ps1
+setLocal
   call :addFileLn .\ps1.constants.txt
   call :addFileLn .\ps1.try.txt
     call :addFileLn .\ps1.example.txt
   call :addFileLn .\ps1.finally.txt
-
-goto :exit
+goto :return
 
 :clear
 setLocal
-type nul >"%outfolder%\%outfile%"
-REM copy /b nul "%outfolder%\%outfile%"
+  type nul >"%outfolder%\%outfile%"
+  REM copy /b nul "%outfolder%\%outfile%"
 goto :return
 
 :toBuffer
 setLocal
-del "%outfolder%\%outfile%.temp" >nul 2>&1
-ren "%outfolder%\%outfile%" "%outfile%.temp"
-call :clear
+  del "%outfolder%\%outfile%.temp" >nul 2>&1
+  ren "%outfolder%\%outfile%" "%outfile%.temp"
+  call :clear
 endlocal & (set buffer=%outfile%.temp) & exit /b 0
 
 :clearBuffer
 setLocal
-del "%outfolder%\%buffer%" >nul 2>&1
+  del "%outfolder%\%buffer%" >nul 2>&1
 goto :return
 
 :addLn
 setLocal
-(echo.)>>"%outfolder%\%outfile%"
+  (echo.)>>"%outfolder%\%outfile%"
 goto :return
 
 :addFile
 setLocal
-if [%1]==[] goto :error
-copy /b "%outfolder%\%outfile%" + "%1" "%outfolder%\%outfile%" >nul
+  if [%1]==[] goto :error
+  copy /b "%outfolder%\%outfile%" + "%1" "%outfolder%\%outfile%" >nul
 goto :return
 
 :addFileLn
 setLocal
-if [%1]==[] goto :error
-copy /b "%outfolder%\%outfile%" + "%1" "%outfolder%\%outfile%" >nul
-(echo.)>>"%outfolder%\%outfile%"
+  if [%1]==[] goto :error
+  copy /b "%outfolder%\%outfile%" + "%1" "%outfolder%\%outfile%" >nul
+  (echo.)>>"%outfolder%\%outfile%"
 goto :return
 
 :prefixFile
 setLocal
-if [%1]==[] goto :error
-call :toBuffer
-call :addFile "%1"
-call :addFile "%outfolder%\%buffer%"
-call :clearBuffer
+  if [%1]==[] goto :error
+  call :toBuffer
+  call :addFile "%1"
+  call :addFile "%outfolder%\%buffer%"
+  call :clearBuffer
 goto :return
 
 :addString
 setLocal
-if [%1]==[] goto :error
-<nul set /p ".=%1" >>"%outfolder%\%outfile%"
+  if [%1]==[] goto :error
+  <nul set /p ".=%1" >>"%outfolder%\%outfile%"
 goto :return
 
 :addStringLn
 setLocal
-if [%1]==[] goto :error
-<nul set /p ".=%1" >>"%outfolder%\%outfile%"
-(echo.)>>"%outfolder%\%outfile%"
+  if [%1]==[] goto :error
+  <nul set /p ".=%1" >>"%outfolder%\%outfile%"
+  (echo.)>>"%outfolder%\%outfile%"
 goto :return
 
 :prefixString
 setLocal
-if [%1]==[] goto :error
-call :toBuffer
-echo|(set /p=%1) >>"%outfolder%\%outfile%"
-call :addFile "%outfolder%\%buffer%"
-call :clearBuffer
+  if [%1]==[] goto :error
+  call :toBuffer
+  echo|(set /p=%1) >>"%outfolder%\%outfile%"
+  call :addFile "%outfolder%\%buffer%"
+  call :clearBuffer
 goto :return
 
 :everyLinePrefixFile
 setLocal disabledelayedexpansion
 if [%1]==[] goto :error
-call :toBuffer
-for /f "usebackq delims=" %%a in ("%outfolder%\%buffer%") do (
-  call :addFile "%1"
-  echo.%%a >>"%outfolder%\%outfile%"
-)
-call :clearBuffer
+  call :toBuffer
+  for /f "usebackq delims=" %%a in ("%outfolder%\%buffer%") do (
+    call :addFile "%1"
+    echo.%%a >>"%outfolder%\%outfile%"
+  )
+  call :clearBuffer
 goto :return
 
 REM ################
@@ -118,9 +126,9 @@ goto :exit
 
 :example
 setLocal
-if [%1]==[] goto :error
-(set var1=%1)
-(set return=%var1% world)
+  if [%1]==[] goto :error
+  (set var1=%1)
+  (set return=%var1% world)
 goto :return
 
 :error
