f566b3416eed78cdcece09d43dc7368f8af57b7e
RefinedSoftwareLLC <mikar_gibbros@yahoo.com>
2018-10-12 04:54:50 -0600

    compile helper functions working

--- source/compile.cmd
+++ source/compile.cmd
@@ -1,19 +1,96 @@
-@ setlocal & echo off
+@ setLocal enableDelayedExpansion & echo off
 
 REM #################
 REM ### .cmd mode ###
 REM #################
 
 cd %~dp0
-echo.passed: %*
-REM copy /b NUL EmptyFile.txt
+(set outfolder=..)
+(set outfile=test.sh.bat)
 
+call :clear
+call :addFileLn .\file.prefix.bat.txt
+call :addFile .\file.prefix.bat.txt
+call :everyLinePrefixFile .\comment.prefix.run.bat.txt
+call :prefixString "bob"
+
+REM type "%outfolder%\%outfile%"
 goto :exit
 
-:append
-if [%1]==[] goto :error
-set file=%1
+:clear
+setLocal
+type nul >"%outfolder%\%outfile%"
+REM copy /b nul "%outfolder%\%outfile%"
+REM echo|(set /p=) >"%outfolder%\%outfile%"
+goto :return
 
+:toBuffer
+setLocal
+del "%outfolder%\%outfile%.temp" >nul 2>&1
+ren "%outfolder%\%outfile%" "%outfile%.temp"
+call :clear
+endlocal & (set buffer=%outfile%.temp) & exit /b 0
+
+:clearBuffer
+setLocal
+del "%outfolder%\%buffer%" >nul 2>&1
+goto :return
+
+:addLn
+setLocal
+(echo.)>>"%outfolder%\%outfile%"
+goto :return
+
+:addFile
+setLocal
+if [%1]==[] goto :error
+copy /b "%outfolder%\%outfile%" + "%1" "%outfolder%\%outfile%" >nul
+goto :return
+
+:addFileLn
+setLocal
+if [%1]==[] goto :error
+copy /b "%outfolder%\%outfile%" + "%1" "%outfolder%\%outfile%" >nul
+(echo.)>>"%outfolder%\%outfile%"
+goto :return
+
+:prefixFile
+setLocal
+if [%1]==[] goto :error
+copy /b "%1" + "%outfolder%\%outfile%" "%outfolder%\%outfile%" >nul
+goto :return
+
+:addString
+setLocal
+if [%1]==[] goto :error
+echo|(set /p=%1) >>"%outfolder%\%outfile%"
+goto :return
+
+:addStringLn
+setLocal
+if [%1]==[] goto :error
+echo|(set /p=%1) >>"%outfolder%\%outfile%"
+(echo.)>>"%outfolder%\%outfile%"
+goto :return
+
+:prefixString
+setLocal
+if [%1]==[] goto :error
+call :toBuffer
+echo|(set /p=%1) >>"%outfolder%\%outfile%"
+call :addFile "%outfolder%\%buffer%"
+call :clearBuffer
+goto :return
+
+:everyLinePrefixFile
+setLocal
+if [%1]==[] goto :error
+call :toBuffer
+for /f "usebackq delims=" %%a in ("%outfolder%\%buffer%") do (
+  call :addFile %1
+  echo.%%a >>"%outfolder%\%outfile%"
+)
+call :clearBuffer
 goto :return
 
 REM ################
@@ -23,19 +100,21 @@ REM ################
 goto :exit
 
 :example
-setlocal
+setLocal
 if [%1]==[] goto :error
-set var1=%1
-set return=%var1% world
+(set var1=%1)
+(set return=%var1% world)
 goto :return
 
 :error
 echo.Error: %0 %*
-rem fall-through
+endlocal & (set return=)
+if [%1]==[] exit /b 1
+exit /b %1
 
 :return
-endlocal & set return=%return%
-rem fall-through
+endlocal & (set return=%return%) & exit /b 0
 
 :exit
-exit /B 1
+if [%1]==[] exit /b 0
+exit /b %1
